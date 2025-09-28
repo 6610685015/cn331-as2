5c0dad4c5f181a3e568bf767d5e04bf334a1e7fa
Athichart Penwong <arm280747@gmail.com>
2025-09-28 22:31:52 +0700

    add login and index

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path("", views.login_views, name="Login"),
+    path("login/", views.login_views, name="Login"),
     path("logout/", views.logout_views, name="Logout"),
     path("home/", views.index, name="home"),
+    path("index/", views.index, name="index"),
     path("register/", views.register_views, name="Register"),
 ]
+
